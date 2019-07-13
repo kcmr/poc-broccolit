9d454e349905fc92a87b92f1ebf97833817335c5
Kus Cámara <kcmr@users.noreply.github.com>
2019-07-14 01:42:11 +0200

    feat(all): add live reload and rollup config

--- Brocfile.js
+++ Brocfile.js
@@ -2,12 +2,12 @@
 
 const funnel = require('broccoli-funnel');
 const mergeTrees = require('broccoli-merge-trees');
-const Rollup = require('broccoli-rollup');
 const BroccoliStyleImporter = require('./lib/broccoli-styles');
+const Rollup = require('broccoli-rollup');
+const rollupConfig = require('./lib/rollup-config');
+const BrowserSync = require('broccoli-bs');
 
 const srcDir = 'src';
-const demoDir = 'demo';
-
 const srcScripts = funnel(srcDir, {
   include: ['**/*.js'],
   exclude: ['**/*.css']
@@ -21,5 +21,21 @@ const demo = funnel('.', {
 
 const styleScripts = new BroccoliStyleImporter(scrStyles);
 const scripts = mergeTrees([styleScripts].concat(srcScripts));
+const bundle = new Rollup(
+  scripts,
+  rollupConfig({
+    input: 'component.js',
+    output: 'component.mjs'
+  })
+);
 
-module.exports = mergeTrees([scripts, demo]);
+const devBundle = mergeTrees([demo, bundle]);
+const devServer = new BrowserSync(devBundle, {
+  bs: {
+    open: false,
+    notify: false
+  }
+});
+
+// module.exports = devBundle;
+module.exports = mergeTrees([devBundle, devServer]);
--- src/component.js
+++ src/component.js
@@ -4,7 +4,8 @@ import style from './component.css';
 class Component extends LitElement {
   static get properties() {
     return {
-      greeting: { type: String }
+      greeting: { type: String },
+      items: { type: Array }
     };
   }
 
@@ -12,15 +13,39 @@ class Component extends LitElement {
     super();
 
     this.greeting = 'Hello world';
+    this.items = [
+      {
+        name: 'hola'
+      },
+      {
+        name: 'mundo'
+      }
+    ];
+  }
+
+  _onButtonClick() {
+    this.items = this.items.concat({
+      name: `item ${this.items.length + 1}`
+    });
   }
 
   static get styles() {
-    return css`${unsafeCSS(style)}`;
+    return css`
+      ${unsafeCSS(style)}
+    `;
   }
 
   render() {
     return html`
       <p>${this.greeting}</p>
+      <button @click=${this._onButtonClick}>Add item</button>
+      <ul>
+        ${this.items.map(
+          item => html`
+            <li>${item.name}</li>
+          `
+        )}
+      </ul>
     `;
   }
 }
